b18c6b2f87fee86233d171761c96b201eac6b91a
Anno Zhang <anno100029090@gmail.com>
2021-02-19 03:53:22 -0500

    Added dataset

--- app.R
+++ app.R
@@ -1,49 +1,42 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#Homework1
+#Anno Zhang
+#AndrewID: zhilianz
+
 
 library(shiny)
-
+library(ggplot2)
+library(dplyr)
+data <- read.csv("data.csv",col.names = c("State", "Cases","Deaths","Fatality","WeeklyNewCases","VaccineAdministered"))
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Public Safety in Pittsburgh"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("type",
+                  "Select the data you want to view",
+                  choices = c("MajorCrimeReports","Non-majorCrimeReports","OtherPoliceReports","SumofCrime","CrimePer100Person")
+      )
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("plot")
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$plot <- renderPlot({
+      ggplot(data, aes(data$Neighborhood, data$CrimePer100Person), colur = "red")
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
