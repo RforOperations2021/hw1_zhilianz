9141aa311990a5a33e76818ba72cc2418c0621c2
Anno Zhang <anno100029090@gmail.com>
2021-02-19 19:16:01 -0500

    buttons added plots added table added

--- app.R
+++ app.R
@@ -6,35 +6,102 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(DT)
+library(shinyBS)
+
+#Read data from csv file
 data <- read.csv("data.csv",col.names = c("State", "Cases","Deaths","Fatality","WeeklyNewCases","VaccineAdministered"))
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Public Safety in Pittsburgh"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("type",
-                  "Select the data you want to view",
-                  choices = c("MajorCrimeReports","Non-majorCrimeReports","OtherPoliceReports","SumofCrime","CrimePer100Person")
-      )
-    ),
     
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("plot")
+    # Application title
+    titlePanel("COVID-19 CASES IN US"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            #Slider input to determine sample size
+            sliderInput("size",
+                        "How many states you want to select",
+                        min=1, 
+                        max = 40,
+                        value = 10),
+            
+            #Radio button input to select data genre
+            radioButtons("type",
+                         "Select data you want to view",
+                         choices = c("Cases","Deaths","Fatality","7 day New Cases","Vaccine Administered")
+            ),
+            
+            #checkbox input to display table
+            checkboxInput("table","Show Table",value = FALSE),
+            
+            #download button to download dataframe into csv file
+            downloadButton("download","Download Data")
+            
+            
+        ),
+        
+        mainPanel(
+            plotOutput("plot"),
+            DT::dataTableOutput("table")
+            
+        )
     )
-  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  
-  output$plot <- renderPlot({
-      ggplot(data, aes(data$Neighborhood, data$CrimePer100Person), colur = "red")
-  })
+    
+    observeEvent(input$size,{
+        selected_data <<- data %>% sample_n(input$size, replace = FALSE)
+        
+        #Drawing different plots based on user's selection
+        output$plot <- renderPlot({
+            
+            if(input$type == 'Cases'){
+                slices <- selected_data$Cases
+                pie(slices,label = selected_data$State,main = "Total cases proportion in selected states")
+                
+            }
+            else if(input$type =='Deaths'){
+                
+                ggplot(selected_data, aes(x =  State, y =Deaths)) + geom_col(fill ="pink") +ggtitle("Death cases in selected states")
+                
+            }
+            else if(input$type == 'Fatality'){
+                ggplot(selected_data, aes(x =  Fatality, y = State)) + geom_point()+ggtitle("Fatality rate in selected states")
+                
+            }
+            else if(input$type == '7 day New Cases'){
+                ggplot(selected_data, aes(x = WeeklyNewCases, y = State)) + geom_col(fill ="lightblue")+ggtitle("Weekly new cases in selected states")
+                
+                
+            }
+            else if(input$type == "Vaccine Administered"){
+                ggplot(selected_data, aes(x =  VaccineAdministered, y =State)) + geom_col(fill ="lightgreen")+ggtitle("Vaccine administered in selected states")
+                
+            }
+            
+        })
+        
+        output$table = DT::renderDataTable(
+            if (input$table){
+                DT::datatable(data = selected_data,
+                              options = list(pageLength = 10),
+                              rownames = FALSE
+                )
+            }
+        )
+    })
+    
+    
+    
+    output$download <- downloadHandler(
+        filename = "data.csv",
+        content = function(file) {
+            write.csv(data, file,row.names = FALSE)
+        }
+    )
+    
 }
 
 # Run the application 
